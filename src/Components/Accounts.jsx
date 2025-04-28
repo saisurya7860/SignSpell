6a0e4ced53960d2a7776106b224725070809af43
Sai Surya <saisurya7860@gmail.com>
2025-04-28 17:30:31 +0530

    profile page added

--- src/Components/Accounts.jsx
+++ src/Components/Accounts.jsx
@@ -1,12 +1,173 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { FaCameraRetro, FaEye, FaEyeSlash, FaLocationArrow, FaLock, FaMapMarker, FaMapMarkerAlt, FaSearchLocation, FaSuitcase, FaUnlockAlt, FaUserLock } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+import { FaPen } from 'react-icons/fa';
 
 const Accounts = () => {
     const accountData = {
-        
+        userProfile:{
+          name: 'Surya',
+          Email: '123@gmail.com',
+          memberSince: 'Apr 19, 2025, 3:26 PM',
+          bio:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae in fuga velit quas aliquam cupiditate',
+          position:'heello',
+          company:'abc',
+          loation:'covai',
+        }
     }
+
+    const [editProfile,setEditProfile] = useState(false);
+    const [userImage,setUserImage] = useState(null);
+    const [showPassword,setShowPassword] = useState(false);
+
+    const handleImageChange = (e) => {
+      const file = e.target.files[0];
+      if (file){
+        setUserImage(URL.createObjectURL(file));
+      }
+    };
   return (
-    <div>
-        <div>This is Account content.</div>
+    <div className='p-6 '>
+        <div className='flex flex-col sm:flex-row justify-center sm:justify-start items-center gap-5 pl-6'>
+          <div className="flex items-center justify-center">
+            <label htmlFor="fileInput" className="cursor-pointer flex items-center justify-center rounded-full w-38 h-38 bg-gray-300 hover:bg-gray-400 dark:bg-gray-900  dark:hover:bg-gray-800">
+              {userImage ?(
+                <img src={userImage} alt="Selected user image" className="rounded-full object-cover w-full h-full" />
+              ) : ( <div className='text-2xl'><FaCameraRetro/></div>)}
+              
+              <input
+                id="fileInput"
+                type="file"
+                name="img"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageChange}
+              />
+            </label>
+          </div>
+        
+
+          {/* Username and email div */}
+          <div className='flex flex-col items-start justify-center gap-3'>
+              <p className='text-3xl font-bold'>{accountData.userProfile.name}</p>
+              <div className=" text-gray-500 dark:text-gray-400 flex items-center gap-2">
+                <FaEnvelope/>
+                <p className='font-semibold text-sm text-gray-700 dark:text-gray-400'>{accountData.userProfile.Email}</p>
+              </div>
+              <p className='text-gray-600 text-xs  dark:text-gray-400 '>MemberSince: {accountData.userProfile.memberSince}</p>
+              {!editProfile ? <button className='cursor-pointer  w-30 pl-2 h-10 dark:bg-gray-800 bg-gray-300 rounded-md border flex items-center gap-2 border-gray-400 dark:border-gray-600' onClick={()=> setEditProfile(!editProfile)}><FaPen/> Edit Profile</button>
+              :
+              <div className='space-x-2'> 
+                <button className='cursor-pointer py-1 px-2 bg-gray-300 text-gray-800 dark:text-gray-200 dark:bg-gray-800 border border-gray-400 dark:border-gray-600 rounded' onClick={()=> setEditProfile(!editProfile)}>Cancel</button>
+                <button className='cursor-pointer py-1 px-3 border-gray-600 rounded text-gray-100 bg-blue-500' onClick={()=> setEditProfile(!editProfile)}>Save</button>
+              </div>}
+              
+          </div>
+        </div>
+        
+        {!editProfile && (
+        <div className='border-t border-gray-600 mt-6 p-6  flex flex-col  space-y-6'>
+          <p className='text-lg font-semibold'>Profile Details</p>
+          <div className='text-gray-800 dark:text-gray-300 '>
+            <div className='font-semibold'>Bio</div>
+            <div className='text-sm text-gray-500 dark:text-gray-400'>{accountData.userProfile.bio}</div>
+          </div>
+          <div className=' flex flex-col sm:flex-row gap-4 justify-between'>
+            <div className=''>
+              <p className=' font-semibold mb-2 text-gray-800 dark:text-gray-300'>positon</p>
+              <div className='flex items-center gap-1'>
+                <p className='text-gray-400'><FaSuitcase/></p>
+                <p className='font- text-base italic text-gray-500 dark:text-gray-400'>{accountData.userProfile.position}</p>
+              </div>
+            </div>
+
+            <div className=''>
+              <p className=' font-semibold mb-2 text-gray-800   dark:text-gray-300'>Company</p>
+              <div className='flex items-center gap-1'>
+                <p className='text-gray-400'><FaSuitcase/></p>
+                <p className='font- text-base italic text-gray-00 dark:text-gray-400'>{accountData.userProfile.company}</p>
+              </div>
+            </div>
+
+            <div className=''>
+              <p className=' font-semibold mb-2 text-gray-800  dark:text-gray-300'>Location</p>
+              <div className='flex items-center gap-1'>
+                <p className='text-gray-400'><FaMapMarkerAlt/></p>
+                <p className='font- text-base italic text-gray-00 dark:text-gray-400'>{accountData.userProfile.loation}</p>
+              </div>
+            </div> 
+          </div>
+          
+        </div>
+      )}
+        {editProfile && (
+        <div className='border-t border-gray-600 mt-6 p-6'>
+          <p className='text-lg font-semibold'>Profile Details</p>
+          <div className='mt-3'>
+            <p>Bio</p>
+            <textarea name="Bio" id="" className='outline-none px-2 py-1 bg-gray-300 dark:bg-gray-800 rounded w-full mt-2 h-20'></textarea>
+          </div>
+
+          <div className='flex flex-col sm:flex-row gap-4 justify-between '>
+            <div className='w-full'>
+              <p className='mt-3'>Position</p>
+              <input type="text" className='outline-none px-2 bg-gray-300 dark:bg-gray-800 rounded w-full  mt-2 h-10' />
+            </div>
+
+            <div className='w-full'>
+              <p className='mt-3'>Company</p>
+              <input type="text" className='outline-none px-2 bg-gray-300 dark:bg-gray-800 rounded w-full  mt-2 h-10' />
+            </div>
+
+            <div className='w-full'>
+              <p className='mt-3'>Position</p>
+              <input type="text" className='outline-none px-2 bg-gray-300 dark:bg-gray-800 rounded w-full  mt-2 h-10' />
+            </div>
+          </div>
+        </div>
+      )}
+
+        <div className='mt-6 p-6 bg-gray-100 dark:bg-gray-900 rounded-md border-gray-300 dark:border-gray-600 border'>
+          <div className='flex items-center gap-2 mb-4'>
+            <div className='text-xl font-light text-blue-400'><FaUnlockAlt/></div>
+            <p className='text-lg font-semibold text-gray-800 dark:text-gray-300'>Change Password</p>
+          </div>
+
+          <div className='mt-7 border-t border-gray-600 pt-4 flex flex-col gap-3 lg:flex-row justify-between'>
+                <div>
+                  <div className='mb-2 text-sm text-gray-600'>Current Password</div>
+                  <div className='relative max-w-70'>
+                      <input type="text" className='bg-gray-50 outline-none px-2 dark:bg-gray-600 py-1 w-full rounded border border-gray-500 focus:ring-blue-400 '/>
+                      <button type="button" className=" cursor-pointer absolute inset-y-0 right-2 flex items-center dark:text-gray-400 text-gray-600  dark:hover:text-gray-200" onClick={() =>setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                      </button>
+                  </div>
+                </div>
+
+                <div>
+                  <div className='mb-2 text-sm  text-gray-600'>New Password</div>
+                  <div className='relative max-w-70'>
+                      <input type="text" className='outline-none bg-gray-50 px-2 dark:bg-gray-600 py-1 w-full rounded border border-gray-500 focus:ring-blue-400 '/>
+                      <button type="button" className=" cursor-pointer absolute inset-y-0 right-2 flex items-center dark:text-gray-400 text-gray-600  dark:hover:text-gray-200" onClick={()      =>setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                      </button>
+                  </div>
+                </div>
+
+                <div>
+                  <div className='mb-2 text-sm  text-gray-600'>Confirm Password</div>
+                  <div className='relative max-w-70'>
+                      <input type="text" className='outline-none px-2 bg-gray-50  dark:bg-gray-600 py-1 w-full rounded border border-gray-500 focus:ring-blue-400 '/>
+                      <button type="button" className=" cursor-pointer absolute inset-y-0 right-2 flex items-center dark:text-gray-400 text-gray-600  dark:hover:text-gray-200" onClick={()      =>setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                      </button>
+                  </div>
+                </div>
+          </div>
+        </div>
+          {/* Password div completed */}
+
+        
     </div>
   )
 }
--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
+import { FaChevronLeft } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+import Accounts from '../Components/Accounts';
+
 
 const Profile = () => {
+  const navigate = useNavigate();
   return (
-    <>
-        <div className='p-3 '>
-            <p className='text-2xl font-semibold dark:text-gray-300 text-gray-700'>Profile Page</p>
-            <div className='m-1 h-screen rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300 text-gray-700'>
 
+    <div className='p-3 min-h-screen '>
+        <p className='text-2xl font-semibold dark:text-gray-300 text-gray-700'>Profile Page</p>
+        <div className='m-1 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-300 text-gray-700 border border-gray-300'>
+          <div className='flex items-center justify-between font-semibold p-10'>
+            <div className='flex items-center gap-1 cursor-pointer' onClick={()=> navigate('/')}>
+              <p className='dark:text-gray-300 text-gray-700 font-light text-sm flex items-center'><FaChevronLeft/></p>
+              <p className='dark:text-gray-300 text-gray-800 text-lg  flex items-center'>Back </p>
             </div>
+            <p className='dark:text-gray-300 text-gray-800 text-lg '>  User Profile </p>
+          </div>
+
+          {/* user profile contents div */}
+          <Accounts/>
         </div>
-    </>
+    </div>
+    
   )
 }
 
