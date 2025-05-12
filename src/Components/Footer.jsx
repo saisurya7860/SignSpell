6e9c2a420537299513b82263fb3b2a8063512f39
Sai Surya <saisurya7860@gmail.com>
2025-05-12 12:26:18 +0530

    route to machine maestros updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-gray-50 dark:bg-gray-700 p-2 flex justify-center text-xs'>
-      <div className='text-gray-700 font-semibold dark:text-gray-50'>Designed by <span className='text-green-600 font-bold cursor-pointer'>Machine Maestros</span></div>
+      <div className='text-gray-700 font-semibold dark:text-gray-50'>Designed by <a href="https://saisurya7860.github.io/MachineMaestro/" target='_blank'><span className='text-green-600 font-bold cursor-pointer'>Machine Maestros</span></a></div>
     </div>
   )
 }
